c77504374e0a99a281585ae38c36b3741aebc826
sakib276 <nazmussakib06927@gmail.com>
2025-06-13 01:12:11 +0600

    new solution

--- retailation.cpp
+++ retailation.cpp
@@ -8,33 +8,45 @@ int main()
     cin>>t;
     while(t--)
     {
-        long long int n;
+        long long int n,i;
         cin>>n;
-        long long int ar[n];
-        for(long long int i=0;i<n;i++)
+        long long int A[n];
+        for( i=0; i<n; i++)
         {
-            cin>>ar[i];
+            cin>>A[i];
         }
-        bool flag=true ;
+        bool flag=false ;
         int f=0;
 
-        for(long long int i=0;i<n;i++)
+        for(i=2; i<n; i++)
         {
-            long long int a=i+1;
-            long long int b=n-a+1;
-           if(ar[i]%a!=0 || ar[i]%b!=0)
-           {
-               flag=false;
-               break;
-           }
+            if((A[1]-A[0])!=(A[i]-A[i-1]))
+            {
+                flag = true;
+                break;
+            }
         }
+
         if(flag)
         {
-            cout<<"YES"<<endl;
+            cout<<"no"<<endl;
+            continue;
+        }
+
+        if((2*A[0]-A[1])>=0 && (A[0]+(A[1]-A[0])*n)>=0)
+        {
+            if((2*A[0]-A[1])%(n+1)==0)
+            {
+                cout<<"yes"<<endl;
+            }
+            else
+            {
+                cout<<"no"<<endl;
+            }
         }
         else
         {
-            cout<<"NO"<<endl;
+            cout<<"no"<<endl;
         }
     }
 }
