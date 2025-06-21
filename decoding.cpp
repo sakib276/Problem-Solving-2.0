275591f96a191095d1ef30f0adfc83e9baca05bd
sakib276 <nazmussakib06927@gmail.com>
2025-06-22 00:04:35 +0600

    added solution updated

--- decoding.cpp
+++ decoding.cpp
@@ -1,51 +1,56 @@
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int n;
-    cin>>n;
-    string s,str;
-    cin>>s;
-    int a,mid;
-    if(n%2)
+    cin >> n;
+    string s, str;
+    cin >> s;
+    str.resize(n);  
+    int a, mid;
+
+    if (n % 2) 
     {
-         a=1;
-         mid= (n-1)/2;
-        for(int i=0;i<n;i++)
+        a = 1;
+        mid = (n - 1) / 2;
+        for (int i = 0; i < n; i++)
         {
-            if(i%2==0)
+            if (i % 2 == 0)
             {
-                str[mid]=s[i];
-                mid=mid-a;
+                str[mid] = s[i];
+                mid = mid - a;
             }
             else
             {
-                str[mid]=s[i];
-                mid=mid+a;
+                str[mid] = s[i];
+                mid = mid + a;
             }
             a++;
         }
     }
-    else
+    else 
     {
-        a=1;
-        mid=(n-1)/2;
-        for(int i=0;i<n;i++)
+        a = 1;
+        mid = (n - 1) / 2;
+        for (int i = 0; i < n; i++)
         {
-            if(i%2==0)
+            if (i % 2 == 0)
             {
-                str[mid]=s[i];
-                mid=mid+a;
+                str[mid] = s[i];
+                mid = mid + a;
             }
             else
             {
-                str[mid]=s[i];
-                mid=mid-a;
+                str[mid] = s[i];
+                mid = mid - a;
             }
             a++;
         }
     }
-  
-      cout<<str<<endl;
+
+    cout << str << endl;
 }
+
 
