7b877e877b9ebc9bcd197ac7abcadaf8f1b80639
sakib276 <nazmussakib06927@gmail.com>
2025-06-25 23:56:16 +0600

    not solved yet

---
+++ K-th_Not_Divisible_by_n.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n,k;
+        cin>>n>>k;
+        long long int a,b,c,d;
+        a=abs(n-1);
+        
+    }
+}
