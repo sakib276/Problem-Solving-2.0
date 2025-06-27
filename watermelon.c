30b531b725cb5a3a7a037559efb8b01485582dd5
sakib276 <nazmussakib06927@gmail.com>
2025-06-27 23:53:00 +0600

    solved in c

---
+++ watermelon.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int w ;
+  scanf("%d",&w);
+  if(w==2){
+      printf("no");
+                  }
+ else  if(w%2==0){
+    printf("YES");
+   }
+
+
+    else {
+     printf("no") ;
+
+   }
+    return 0 ;
+
+}
