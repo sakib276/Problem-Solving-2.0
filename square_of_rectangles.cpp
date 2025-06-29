1c780d054334777ad6777253b07b1d710d46e5db
sakib276 <nazmussakib06927@gmail.com>
2025-06-30 00:53:26 +0600

    problem is solved

--- square_of_rectangles.cpp
+++ square_of_rectangles.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -13,13 +12,13 @@ int main()
         cin>>l1>>b1>>l2>>b2>>l3>>b3;
         int a= max(l1, max(l2, l3));
         int b= max(b1, max(b2, b3));
-        if(l1==l2==l3==b1==b2==b3)
+        if((l1==l2)&& (l1==l3)&& (l1==b1)&&(l1==b2)&&(l1==b3))
         {
             cnt=0;
         }
         else if(a>b)
         {
-            if(l1==l2==l3)
+             if((l1==l2) &&(l1==l3))
             {
                 if(l1==(b1+b2+b3))
                 {
@@ -27,80 +26,43 @@ int main()
                 }
 
             }
-            else
+            else if((l2+l3 == l1 && b2==b3&& b1+b2==l1))
             {
-                if(l1==a)
-                {
-                    e=l2+l3;
-                    if(e==l1)
-                    {
-                        cnt++;
-                    }
-                }
-                else if(l2==a)
-                {
-                    e=l1+l3;
-                    if(e==l2)
-                    {
-                        cnt++;
-                    }
-                }
-                else if(l3==a)
-                {
-                    e=l2+l1;
-                    if(e==l3)
-                    {
-                        cnt++;
-                    }
-                }
+
+
+                    cnt++;
+
             }
         }
         else
         {
-                if(b1==b2==b3)
+
+            if((b1==b2) &&(b1==b3))
             {
-                if(b1==(l1+l2+l3))
+
+                if(b==(l1+l2+l3))
                 {
                     cnt++;
+
                 }
 
             }
-            else
+            else if((b1==b2+b3 && l2==l3 && l1+l2==b1))
             {
-                 if(b1==b)
-                {
-                    e=b2+b3;
-                    if(e==b1)
-                    {
-                        cnt++;
-                    }
-                }
-                else if(b2==b)
-                {
-                    e=b1+b3;
-                    if(e==b2)
-                    {
-                        cnt++;
-                    }
-                }
-                else if(b3==b)
-                {
-                    e=b2+b1;
-                    if(e==b3)
-                    {
-                        cnt++;
-                    }
-                }
+                    cnt++;
+
+
             }
         }
-     if(cnt==0)
-     {
-         cout<<"NO"<<endl;
-     }
-     else
-     {
-         cout<<"YES"<<endl;
-     }
+        if(cnt==0)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
     }
+    return 0;
 }
 
