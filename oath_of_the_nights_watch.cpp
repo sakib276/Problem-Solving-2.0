443cc6993107a9948a173bab0659b32f4c764cf0
sakib276 <nazmussakib06927@gmail.com>
2025-07-10 23:57:50 +0600

    solved the problem

--- oath_of_the_nights_watch.cpp
+++ oath_of_the_nights_watch.cpp
@@ -2,33 +2,36 @@
 using namespace std;
 int main()
 {
-    
-        int n;
-        cin>>n;
-        int arr[n];
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-        }
-        int cnt=0;
-        sort(arr, arr + n);
-        if(n==2 || n<2)
-        {
-            cout<<0<<endl;
-        }
-        else
-        {
-            for(int i=1;i<n-1;i++)
-            {
-                if((arr[i]<arr[i+1] && arr[i]!=arr[i+1]) && (arr[i]>arr[i-1] && arr[i]!=arr[i-1]))
-                {
-                    cnt++;
-                }
 
+    long long int n;
+    cin>>n;
+    long long  int arr[n];
+    for(long long int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    long long int cnt=0;
+    sort(arr, arr + n);
+    long long int left= arr[0];
+    long long int right = arr[n-1];
+
+    if(n==2 || n<2)
+    {
+        cout<<0<<endl;
+    }
+    else
+    {
+        for(long long int i=0; i<n; i++)
+        {
+
+            if(left< arr[i] && right>arr[i])
+            {
+                cnt++;
             }
-         cout<<cnt<<endl;
         }
-       
-    
+        cout<<cnt<<endl;
+    }
+
+
 }
 
