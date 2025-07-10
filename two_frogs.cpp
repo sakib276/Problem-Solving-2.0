28fba13ab83245dd0fe603c89b74a4b03ec34621
sakib276 <nazmussakib06927@gmail.com>
2025-07-11 00:38:30 +0600

    solved the problem

---
+++ two_frogs.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,a,b;
+        cin>>n>>a>>b;
+
+        if(abs(a-b)%2==0)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+}
+
