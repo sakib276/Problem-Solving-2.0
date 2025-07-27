c85624cc5a98a5b987cb9cf13c5ebfc316f88ed5
sakib276 <nazmussakib06927@gmail.com>
2025-07-27 22:36:07 +0600

    solved the problem

---
+++ skibidus_and_among_u.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int n=s.size();
+        s[n-2]='i';
+        s[n-1]=' ';
+        cout<<s<<endl;
+
+    }
+}
+
+
