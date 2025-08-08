8c34b7c0a6474a6e6c8364f82f43fa2932d81f0a
sakib276 <nazmussakib06927@gmail.com>
2025-08-08 11:32:37 +0600

    solved newly

--- recyling_center.cpp
+++ recyling_center.cpp
@@ -1,46 +1,40 @@
+<<<<<<< HEAD
 
-#include<bits/stdc++.h>
+    =======
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 using namespace std;
-int main()
-{
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        long long int n,k,ans=0,x=1;
-        cin>>n>>k;
-       vector<long long int>ar(n);
-        for(long long int i=0;i<n;i++)
-        {
-            cin>>ar[i];
+    cin >> t;
+    while (t--) {
+        long long n, k, ans = 0, x = 1;
+        cin >> n >> k;
+        vector<long long> ar(n);
+        for (long long i = 0; i < n; i++) {
+            cin >> ar[i];
         }
-
-        multiset<long long> mp;
-
-        for(auto it: ar){
-            mp.insert(it);
-        }
-        long long y= upper_bound(all(ar),k)-ar.begin();
-        ans=ans+(n-y);
+        sort(all(ar));
+        long long y = upper_bound(all(ar), k) - ar.begin();
+        ans += (n - y);
         y--;
-        if(y<0)
-        {
-            cout<<n<<endl;
+        if (y < 0) {
+            cout << n << endl;
             continue;
         }
-
-        for(long long i=y;i>=0;i--)
-        {
-            if(x*ar[i]>k)
-            {
+        for (long long i = y; i >= 0; i--) {
+            if (x * ar[i] > k) {
                 ans++;
-            }
-            else {
-                x=x*2;
+            } else {
+                x *= 2;
             }
         }
-        cout<<ans<<endl;
+        cout << ans << endl;
+>>>>>>> ead8038 (newly solved)
     }
 }
 
